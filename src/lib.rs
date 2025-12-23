a05e17609f4cc78fbcc322b81fae9c7c03852cf1
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-24 00:47:53 +0100

    feat: add ready()

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use std::pin::Pin;
 use std::task::{Context, Poll};
 
 /// A future that is immediately ready with a value.
+#[allow(dead_code)]
 struct Ready<T> {
     value: Option<T>,
 }
@@ -14,8 +15,13 @@ impl<T> Unpin for Ready<T> {}
 impl<T> Future for Ready<T> {
     type Output = T;
 
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
         let value = self.get_mut().value.take().expect("");
         Poll::Ready(value)
     }
+}
+
+#[allow(dead_code)]
+fn ready<T>(value: T) -> Ready<T> {
+    Ready { value: Some(value) }
 }

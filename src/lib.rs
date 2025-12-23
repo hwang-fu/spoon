18f30ef22341094f6c53ee8b7ee3389660d3f52d
hwang-fu <junzhe.hwangfu@gmail.com>
2025-12-24 00:40:19 +0100

    feat: implement Future for Ready

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,18 @@
 // Spoon async runtime
 
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+
 /// A future that is immediately ready with a value.
 struct Ready<T> {
     value: Option<T>,
+}
+
+impl<T> Future for Ready<T> {
+    type Output = T;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        // TODO: take the value and return Ready
+    }
 }
